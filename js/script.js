26a778d8b88814ff3cdd0eae0128928141ade35c
Vache <vachevirabyan1995@gmail.com>
2021-12-14 18:43:20 +0400

    task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 "use strict";
+
+$(function(){
+    $(window).on('scroll',function(){
+        if($(window).scrollTop() > 0){
+            $('.header').addClass('transfor')
+        }else if ($(window).scrollTop() == 0){
+            $('.header').removeClass('transfor')
+        }
+    })
+})
+
+$(document).ready(function(){
+    $('.slidertop').slick({
+ infinite: true,
+ speed: 300,
+ slidesToShow: 1,
+ centerMode: true,
+ variableWidth: true,
+ speed:1000,
+ autoplay: true,
+ autoplaySpeed: 2000,
+    })
+})
+
+$(document).ready(function(){
+    $('.sliderone').slick({
+ infinite: true,
+ speed: 300,
+ slidesToShow: 1,
+ centerMode: true,
+ variableWidth: true,
+ speed:1000,
+    })
+})
+
+$(document).ready(function(){
+    $('.slidertwo').slick({
+ infinite: true,
+ speed: 300,
+ slidesToShow: 1,
+ centerMode: true,
+ variableWidth: true,
+ speed:1000,
+    })
+})
+
+$(document).ready(function(){
+    $('.sliderfor').slick({
+ infinite: true,
+ speed: 300,
+ slidesToShow: 1,
+ centerMode: true,
+ variableWidth: true,
+ speed:1000,
+    })
+})
